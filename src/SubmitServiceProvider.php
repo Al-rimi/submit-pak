17bf97249787403edbf46bc8d3366ace1c06215a
Al-rimi <ggak71@gmail.com>
2024-11-19 04:46:44 +0800

    fix seeders add migrations

--- src/SubmitServiceProvider.php
+++ src/SubmitServiceProvider.php
@@ -10,7 +10,7 @@ class SubmitServiceProvider extends ServiceProvider
     {
 
         if (method_exists($this->app, 'make') && $this->app->bound('Illuminate\Database\Eloquent\Factory')) {
-            $this->app->make('Illuminate\Database\Eloquent\Factory')->load(__DIR__ . '/Database/Factories');
+            $this->app->make('Illuminate\database\Eloquent\Factory')->load(__DIR__ . '/database/dactories');
         }
 
         $this->publishes([
@@ -20,7 +20,8 @@ class SubmitServiceProvider extends ServiceProvider
             __DIR__ . '/resources/css' => resource_path('css'),
             __DIR__ . '/resources/js' => resource_path('js'),
             __DIR__ . '/resources/images' => public_path('images'),
-            __DIR__ . '/seeders' => database_path('seeders'),
+            __DIR__ . '/database/migrations' => database_path('migrations'),
+            __DIR__ . '/database/seeders' => database_path('seeders'),
         ], 'laravel-assets');
 
     }
